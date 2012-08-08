c287df4cd7e0bb773f53d8e1fdaf6dd9d4dc296a
Freewind <nowind_lee@qq.com>
2012-08-08 23:23:08 +0800

    Code of AviPlayer_PictureBox

---
+++ AviPlayer_PictureBox/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace AviPlayer_PictureBox {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.MinimumSize = new System.Drawing.Size(484, 356);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(484, 356);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 3);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(68, 25);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "打开";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.trackBar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 232);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(484, 70);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnNext);
+            this.panel2.Controls.Add(this.btnPrev);
+            this.panel2.Controls.Add(this.btnStop);
+            this.panel2.Controls.Add(this.btnStart);
+            this.panel2.Controls.Add(this.btnOpen);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 36);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(484, 34);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNext.Enabled = false;
+            this.btnNext.Location = new System.Drawing.Point(315, 4);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "后一帧";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPrev.Enabled = false;
+            this.btnPrev.Location = new System.Drawing.Point(228, 4);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 4;
+            this.btnPrev.Text = "前一帧";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(398, 4);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(145, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar
+            // 
+            this.trackBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trackBar.Location = new System.Drawing.Point(0, 0);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(484, 70);
+            this.trackBar.TabIndex = 2;
+            this.trackBar.Scroll += new System.EventHandler(this.trackBar_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(484, 302);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.MinimumSize = new System.Drawing.Size(500, 340);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ AviPlayer_PictureBox/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace AviPlayer_PictureBox {
+
+    public partial class Form1 : Form {
+
+        IntPtr capture;
+        Timer myTimer;
+        MovieInfo movieInfo;
+
+        delegate void MovieHandler(object sender, MovieEvent e);
+        event MovieHandler MovieHandlers;
+
+        public Form1() {
+            InitializeComponent();
+            this.MovieHandlers += new MovieHandler(this.handler);
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "视频(*.avi)|*.avi";
+            if (dialog.ShowDialog() == DialogResult.OK) {
+                var file = dialog.FileName;
+                capture = CvInvoke.cvCreateFileCapture(file);
+
+                movieInfo = new MovieInfo(file, capture);
+                trackBar.SetRange(0, movieInfo.frameCount);
+                if (myTimer != null) {
+                    myTimer.Stop();
+                    myTimer.Dispose();
+                }
+                myTimer = new Timer();
+                myTimer.Interval = 1000 / Convert.ToInt32(movieInfo.fps);
+                myTimer.Tick += new EventHandler(MyTimer_Tick);
+
+                if (MovieHandlers != null) {
+                    MovieHandlers(this, new MovieEvent(MovieEvent.State.NewMovie));
+                }
+
+                myTimer.Start();
+            }
+        }
+
+        private void MyTimer_Tick(object sender, EventArgs e) {
+            int p = movieInfo.currentFrame + 1;
+            if (p >= movieInfo.frameCount) {
+                myTimer.Stop();
+                renderFrame(0);
+                if (MovieHandlers != null) {
+                    MovieHandlers(this, new MovieEvent(MovieEvent.State.Stopped));
+                }
+                return;
+            }
+            renderFrame(p);
+            if (MovieHandlers != null) {
+                MovieHandlers(this, new MovieEvent(MovieEvent.State.Playing));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            if (myTimer == null) return;
+            myTimer.Stop();
+            renderFrame(0);
+            if (MovieHandlers != null) {
+                MovieHandlers(this, new MovieEvent(MovieEvent.State.Stopped));
+            }
+        }
+
+        private void renderFrame(int p) {
+            if (p < 0) {
+                p = 0;
+            } else if (p >= movieInfo.frameCount) {
+                p = movieInfo.frameCount - 1;
+            }
+            CvInvoke.cvSetCaptureProperty(capture, Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_POS_FRAMES, p);
+            var frame = CvInvoke.cvQueryFrame(capture);
+            Image<Bgr, byte> dest = new Image<Bgr, byte>(movieInfo.width, movieInfo.height);
+            CvInvoke.cvCopy(frame, dest, IntPtr.Zero);
+
+            pictureBox1.Image = dest.ToBitmap();
+            movieInfo.currentFrame = p;
+        }
+
+
+        struct MovieInfo {
+            public String filename;
+            public int frameCount;
+            public int width;
+            public int height;
+            public int currentFrame;
+            public int fps;
+            public bool playing;
+            public MovieInfo(String filename, IntPtr capture) {
+                this.filename = filename;
+                this.frameCount = Convert.ToInt32(CvInvoke.cvGetCaptureProperty(capture, Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_COUNT));
+                this.width = Convert.ToInt32(CvInvoke.cvGetCaptureProperty(capture, Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_WIDTH));
+                this.height = Convert.ToInt32(CvInvoke.cvGetCaptureProperty(capture, Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_HEIGHT));
+                this.currentFrame = Convert.ToInt32(CvInvoke.cvGetCaptureProperty(capture, Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_POS_FRAMES));
+                this.fps = Convert.ToInt32(CvInvoke.cvGetCaptureProperty(capture, Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FPS));
+                this.playing = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if (myTimer == null) return;
+            if (myTimer.Enabled) {
+                myTimer.Stop();
+                MovieHandlers(this, new MovieEvent(MovieEvent.State.Paused));
+            } else {
+                myTimer.Start();
+                MovieHandlers(this, new MovieEvent(MovieEvent.State.Started));
+            }
+        }
+
+        class MovieEvent : EventArgs {
+            public State EventState { get; set; }
+            public enum State {
+                NewMovie, Started, Stopped, Paused, Playing, Scroll, PrevNext
+            }
+            public MovieEvent(State state)
+                : base() {
+                this.EventState = state;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            renderFrame(movieInfo.currentFrame - 1);
+            if (MovieHandlers != null) {
+                MovieHandlers(this, new MovieEvent(MovieEvent.State.PrevNext));
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            renderFrame(movieInfo.currentFrame + 1);
+            if (MovieHandlers != null) {
+                MovieHandlers(this, new MovieEvent(MovieEvent.State.PrevNext));
+            }
+        }
+
+        private void handler(object sender, MovieEvent e) {
+            switch (e.EventState) {
+                case MovieEvent.State.NewMovie:
+                    btnStart.Enabled = true;
+                    btnStop.Enabled = true;
+                    btnPrev.Enabled = true;
+                    btnNext.Enabled = true;
+                    btnStart.Text = "暂停";
+                    break;
+                case MovieEvent.State.Paused:
+                    btnStart.Enabled = true;
+                    btnStop.Enabled = true;
+                    btnPrev.Enabled = true;
+                    btnNext.Enabled = true;
+                    btnStart.Text = "开始";
+                    break;
+                case MovieEvent.State.Playing:
+                    btnStart.Enabled = true;
+                    btnStop.Enabled = true;
+                    btnPrev.Enabled = true;
+                    btnNext.Enabled = true;
+                    btnStart.Text = "暂停";
+                    trackBar.Value = movieInfo.currentFrame;
+                    break;
+                case MovieEvent.State.PrevNext:
+                    btnStart.Enabled = true;
+                    btnStop.Enabled = true;
+                    btnPrev.Enabled = true;
+                    btnNext.Enabled = true;
+                    trackBar.Value = movieInfo.currentFrame;
+                    break;
+                case MovieEvent.State.Started:
+                    btnStart.Enabled = true;
+                    btnStop.Enabled = true;
+                    btnPrev.Enabled = true;
+                    btnNext.Enabled = true;
+                    btnStart.Text = "暂停";
+                    break;
+                case MovieEvent.State.Stopped:
+                    btnStart.Enabled = true;
+                    btnStop.Enabled = false;
+                    btnPrev.Enabled = false;
+                    btnNext.Enabled = false;
+                    btnStart.Text = "开始";
+                    trackBar.Value = 0;
+                    break;
+                case MovieEvent.State.Scroll:
+                    btnStart.Enabled = true;
+                    btnStop.Enabled = true;
+                    btnPrev.Enabled = true;
+                    btnNext.Enabled = true;
+                    break;
+                default: break;
+            }
+        }
+
+        private void trackBar_Scroll(object sender, EventArgs e) {
+            renderFrame((sender as TrackBar).Value);
+            if (this.MovieHandlers != null) {
+                MovieHandlers(this, new MovieEvent(MovieEvent.State.Scroll));
+            }
+        }
+
+    }
+
+}
